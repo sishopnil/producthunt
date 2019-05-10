9b0d9882e2671753451f3889c478b329a5a697fd
Shamiul Shopnil <shopnil16@gmail.com>
2019-05-11 04:22:17 +0600

    ProductHunt

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path , include
 from products import views
 from django.conf import settings
 from django.conf.urls.static import static
